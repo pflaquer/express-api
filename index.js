b8ceb23ae41c78cd9144a16bf8de629043c2d449
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-08-11 16:52:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const express = require('express');
-const fs = require('fs');
+var express = require('express'), 
+app = express(),
+routes = require('./routes');
+api = require('./routes/api'),
+port = process.env.PORT || 3000;
 
-const app = express();
+// Page Routes
+app.get('/', routes.index);
 
-app.post('/api/append', async (req, res) => {
-  // Get the data from the request body
-  const data = req.body;
+// API Routes
+app.get('/api/name', api.name);
 
-  // Read the JSON file
-  const file = await fs.readFile('data.json');
-
-  // Append the data to the JSON file
-  const json = JSON.parse(file);
-  json.push(data);
-  const newFile = JSON.stringify(json, null, 2);
-
-  // Write the JSON file
-  await fs.writeFile('data.json', newFile);
-
-  // Successfully appended the data to the JSON file
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
